87ae28b0f56c428763badae8f3f298055304da5f
yuenkuan <yuenkuan@192.168.1.102>
2020-06-25 21:47:26 +0800

    P2Q1 init

---
+++ src/practical2/P2Q1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practical2;
+
+/**
+ *
+ * @author yuenkuan
+ */
+public class P2Q1 {
+    public static void displaySquareRoot(int max){
+        
+        System.out.println("Number   SquareRoot");
+        
+        for(int i=0; i<=max; i+=2){
+            
+            System.out.printf("%d \t %.4f\n" , i , Math.sqrt(i));
+            
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        displaySquareRoot(20);
+      
+    }
+
+}
